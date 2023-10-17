9a090255d262c2cdd96805f8a26800b40ca2ded7
dronzor <95331028+dronzor@users.noreply.github.com>
2023-10-17 11:59:44 +0530

    Update multiple _desease_prediction_system.py

--- multiple desease prediction/multiple _desease_prediction_system.py
+++ multiple desease prediction/multiple _desease_prediction_system.py
@@ -5,11 +5,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/USER/Documents/c_program/python/multiple desease prediction/sav_files/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/USER/Documents/c_program/python/multiple desease prediction/sav_files/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/USER/Documents/c_program/python/multiple desease prediction/sav_files/Parkinsson disease_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('Parkinsson disease_model.sav', 'rb'))
 
 
 
